0712340ec8d03ac47ff347c5ae01b57527dc407d
JEB <jebarrocal@gmail.com>
2020-11-15 14:21:13 +0100

    add websocket & ngx-logger & create general-channel module

--- README.md
+++ README.md
@@ -17,3 +17,12 @@ $ nest new socket-io-tst
 yarn add @nestjs/websockets @nestjs/platform-socket.io
 yarn add -D @types/socket.io
 ```
+5. Add and config Logger `yarn add ngx-logger`
+
+6. Generate `General-Channel` module `nest g mo global-channel`
+
+### Links
+
+* https://en.wikipedia.org/wiki/WebSocket
+* https://socket.io/docs/
+* https://docs.nestjs.com
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
+import { GlobalChannelModule } from './global-channel/global-channel.module';
 
 @Module({
-  imports: [],
+  imports: [
+    LoggerModule.forRoot({
+      level: NgxLoggerLevel.DEBUG,
+      serverLogLevel: NgxLoggerLevel.ERROR
+    }),
+    GlobalChannelModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
